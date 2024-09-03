3a21d8129178988ec129205e2591196376ebe046
v <vendicated@riseup.net>
2024-09-03 03:08:05 +0200

    switch to stable api again

--- src/discord_api.ts
+++ src/discord_api.ts
@@ -1,6 +1,6 @@
 import { getRuntime } from "@astrojs/cloudflare/runtime";
 
-const API_BASE = "https://canary.discord.com/api/v10";
+const API_BASE = "https://discord.com/api/v10";
 const CDN_BASE = "https://cdn.discordapp.com";
 
 export async function sendRequest<T extends object = any>(req: Request, route: string) {
